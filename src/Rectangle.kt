261ab2b948df87640d88f0ce470cfe5dfd542c62
Leighton Darkins <ljdarkins@gmail.com>
2018-05-20 15:59:19 +0200

    LD - Custom Accessors Example

---
+++ src/Rectangle.kt
@@ -0,0 +1,20 @@
+class Rectangle(val height: Int, val width: Int) {
+    val isSquare: Boolean
+        get() {
+            return height == width
+        }
+
+    val isUneven: Boolean
+        get() = height != width
+}
+
+fun main(args: Array<String>) {
+    val square = Rectangle(5,5)
+    val rectangle = Rectangle(10, 5)
+
+    println(square.isSquare)
+    println(square.isUneven)
+
+    println(rectangle.isSquare)
+    println(rectangle.isUneven)
+}
