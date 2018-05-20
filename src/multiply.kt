ec562f8c79be2ace04b4ce20eb9ef96f43d9a0aa
Leighton Darkins <ljdarkins@gmail.com>
2018-05-20 15:16:43 +0200

    LD - Multiply And Divide. Inline And Block

---
+++ src/multiply.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    println("Multiply")
+    println(multiply(1, 2))
+    println(multiply(2, 2))
+    println(multiply(3, 2))
+    println(multiply(4, 2))
+
+    println("Divide")
+    println(divide(1, 2))
+    println(divide(2, 2))
+    println(divide(3, 2))
+    println(divide(4, 2))
+}
+
+fun multiply(num1: Int, num2: Int) = num1 * num2
+
+// must include a return type for Block Body functions
+fun divide(num1: Int, num2: Int): Int {
+    return num1 / num2
+}
