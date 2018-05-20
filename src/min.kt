207204eb402649fb971b461c8f0a2ecd71d29162
Leighton Darkins <ljdarkins@gmail.com>
2018-05-20 15:12:09 +0200

    LD - Inline min Function

---
+++ src/min.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println(min(1, 5))
+    println(min(3, 1))
+    println(min(-4, -8))
+    println(min(10, 2))
+}
+
+fun min(num1: Int, num2: Int): Int = if (num1 < num2) num1 else num2
