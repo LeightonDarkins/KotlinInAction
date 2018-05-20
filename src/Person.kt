2a879c8dfa66a47ff1dbc83f1de44a24a8c1ce08
Leighton Darkins <ljdarkins@gmail.com>
2018-05-20 15:53:06 +0200

    LD - Basic Person Class Example

---
+++ src/Person.kt
@@ -0,0 +1,47 @@
+// JAVA
+
+//public class Person {
+//    private final String name;
+//    private final Boolean isMarried;
+//
+//    public Person(String name, Boolean isMarried) {
+//        this.name = name;
+//        this.isMarried = isMarried
+//    }
+//
+//    public getName() {
+//        return this.name;
+//    }
+
+//    public isMarried() {
+//        retrun this.isMarried;
+//    }
+//}
+
+// KOTLIN
+
+// create a class called Person with an IMMUTABLE VALUE, name
+// and a VARIABLE: isMarried.
+
+class Person(val name:String, var isMarried:Boolean)
+
+fun main(args: Array<String>) {
+    val steve = Person("Steve", true)
+    val matt = Person("Matt", false)
+    val larene = Person("Larene", false)
+
+    println("Is ${steve.name} married? ${steve.isMarried}")
+    println("Is ${matt.name} married? ${matt.isMarried}")
+    println("Is ${larene.name} married? ${larene.isMarried}")
+
+    // cannot reassign name, it's an IMMUTABLE VALUE
+    // matt.name = "Jimmy"
+
+    // can reassign isMarried, it's a VARIABLE
+    matt.isMarried = true
+    larene.isMarried = false
+
+    println("Is ${steve.name} married? ${steve.isMarried}")
+    println("Is ${matt.name} married? ${matt.isMarried}")
+    println("Is ${larene.name} married? ${larene.isMarried}")
+}
